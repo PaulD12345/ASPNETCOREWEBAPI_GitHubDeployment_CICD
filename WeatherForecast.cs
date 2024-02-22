6b7bfdb75e85e98cef2fa1fb955bd67988029999
@PaulD12345 <praathome@yahoo.com>
2024-02-22 00:15:53 -0500

    Add project files.

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace ASPNETCOREWEBAPI_GitHubDeployment_CICD
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
